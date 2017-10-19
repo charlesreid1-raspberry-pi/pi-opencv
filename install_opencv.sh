a26946b202030aade0b21aaa3492606c6a52d1c0
Charles Reid <charlesreid1@gmail.com>
2017-10-19 11:06:23 +0000

    initial commit - install script.

---
+++ install_opencv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+### # This is a meta-package that points to other dev packages.
+### # Try the stuff below first.
+### apt-get -y install libopencv-dev
+
+apt-get -y install libopencv-core-dev
+apt-get -y install libopencv-video-dev
+apt-get -y install libopencv-features2d-dev
+apt-get -y install python-opencv
+
